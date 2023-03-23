12c6256018b204f477c7a3cbbca33b0b9a27db08
Pebble32 <adp14@hi.is>
2023-03-23 13:15:30 +0000

    Klára menu

--- src/main/java/vidmot/Menu.java
+++ src/main/java/vidmot/Menu.java
@@ -1,10 +1,40 @@
 package vidmot;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Menu {
-    public void playHandler(ActionEvent actionEvent) {
+    @FXML
+    private Button fxLoka;
+    @FXML
+    private VBox fxVbox;
+    private final static String TITLE = "Space Invaders!";
+
+    @FXML
+    private void playHandler () throws IOException {
+        Stage stage = (Stage) fxVbox.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(SpaceInvadersApplication.class.getResource("spaceinvaders-view.fxml"));
+        Parent root = fxmlLoader.load();
+        SpaceInvadersController sc = fxmlLoader.getController();
+
+        stage.setTitle(TITLE);
+        Scene scene = new Scene(root, 400, 500);
+        sc.orvatakkar(scene); // kalla á mapping fall fyrir takka
+        stage.setScene(scene);
+        stage.show();
+        sc.hefjaLeik(); // kalla á keyframe fall til að byrja leik
     }
-    public void exitHandler(ActionEvent actionEvent) {
+    @FXML
+    private void exitHandler() {
+        Stage stage = (Stage) fxLoka.getScene().getWindow();
+        stage.close();
     }
 }
