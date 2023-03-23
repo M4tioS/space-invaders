16f0426db788c620be27e229138476322513b420
Pebble32 <adp14@hi.is>
2023-03-23 14:04:51 +0000

    Setting up keys

--- src/main/java/vidmot/Leikbord.java
+++ src/main/java/vidmot/Leikbord.java
@@ -1,6 +1,18 @@
 package vidmot;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class Leikbord extends Pane {
+    @FXML
+    private Geimskip fxGeimskip;
+
+    public Leikbord(){
+        FXML_Lestur.lesa(this, "leikbord.fxml");
+    }
+
+    public Geimskip getShip(){
+        return fxGeimskip;
+    }
 }
--- src/main/java/vidmot/SpaceInvadersController.java
+++ src/main/java/vidmot/SpaceInvadersController.java
@@ -5,7 +5,10 @@ import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollToEvent;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
 import vinnsla.Data;
 import vinnsla.Game;
@@ -15,7 +18,8 @@ import java.util.HashMap;
 
 public class SpaceInvadersController {
 
-
+    @FXML
+    private Leikbord leikbord;
     private final Game game = new Game();
     private static final HashMap<KeyCode, Direction> map = new HashMap<>();
     private Timeline t;
@@ -26,18 +30,38 @@ public class SpaceInvadersController {
     }
 
     public void orvatakkar(Scene scene) {
+        map.put(KeyCode.RIGHT, Direction.RIGHT);
+        map.put(KeyCode.LEFT, Direction.LEFT);
+        scene.addEventFilter(KeyEvent.ANY,
+                event -> {
+                    try {
+                        if ((event.getCode()) == KeyCode.RIGHT){
+                            setStefna(Direction.RIGHT, 5);
+                        } else if ((event.getCode()) == KeyCode.LEFT){
+                            setStefna(Direction.LEFT, 5);
+                        }
+
+                    } catch (NullPointerException e){
+                        event.consume();
+                    }
+
+        });
     }
 
     public void hefjaLeik() {
         game.newGame();
         KeyFrame k = new KeyFrame(Duration.millis(game.getIntervall()), event -> {
-
+            setStefna(Direction.DOWN, 5);
         });
+        t = new Timeline(k);
+        t.setCycleCount(Timeline.INDEFINITE);
+        t.play();
     }
 
     public void setStefna(Direction d, int x){
         for (int i = 0; i < x; i++){
-
+            leikbord.getShip().setRotate(d.getDeg());
+            leikbord.getShip().moveShip();
         }
     }
 }
