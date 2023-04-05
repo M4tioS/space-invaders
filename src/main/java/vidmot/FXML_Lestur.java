0388d5304735ba53ff7ef9e9b3c1d04dea6bb14b
MatioS <dkm2@hi.is>
2023-04-05 09:54:33 +0000

    test2

--- src/main/java/vidmot/FXML_Lestur.java
+++ src/main/java/vidmot/FXML_Lestur.java
@@ -11,6 +11,7 @@ import java.io.IOException;
  *  Lýsing  : Lesa fxml skrá
  *****************************************************************************/
 public class FXML_Lestur {
+    //test
     public static void lesa(Object controller, String fxmlSkra) {
         FXMLLoader fxmlLoader = new FXMLLoader(controller.getClass().getResource(fxmlSkra));
         fxmlLoader.setClassLoader(controller.getClass().getClassLoader()); // EÞH viðbót fyrir hreiðraða sérhæfða klasa - sjá hér lausn https://stackoverflow.com/questions/50482659/scene-builder-nested-custom-nodes/50493549#50493549
