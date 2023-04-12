875b5134e98a897809ee32ba300cd2a2ab97d9c0
matio <dkm2@hi.is>
2023-04-12 11:51:17 +0000

    Bæta leturgerð

--- src/main/java/vidmot/SpaceInvadersApplication.java
+++ src/main/java/vidmot/SpaceInvadersApplication.java
@@ -5,9 +5,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class SpaceInvadersApplication extends Application {
     public static final String TITLE = "Space Invaders!";
